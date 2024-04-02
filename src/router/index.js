d910ffbfb596e648436a604a6d0cb0d2df245d30
hung vuong <hungvq.nhs@gmail.com>
2024-04-02 23:46:17 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,5 @@
 import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
-import router from "./router";
-
-createApp(App).use(router).mount("#app");
 
 const routes = [
     {
@@ -10,10 +7,21 @@ const routes = [
         name: "contactbook",
         component: ContactBook,
     },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    },
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true // Truyền các biến trong $route.params vào làm props
+    },
 ];
-
 const router = createRouter({
-
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
 });
 
 export default router;
